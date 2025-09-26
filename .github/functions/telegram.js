842c22ede52b96f91e4e4f4974ba7b0b482753c5
joaoaviz <87193061+joaoaviz@users.noreply.github.com>
2025-09-26 15:40:22 +0200

    Update telegram.js

--- .github/functions/telegram.js
+++ .github/functions/telegram.js
@@ -5,7 +5,7 @@ export const onRequestOptions = async () => {
     status: 204,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
       'Access-Control-Allow-Headers': 'Content-Type'
     }
   });
